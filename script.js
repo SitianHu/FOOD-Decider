0875310d2e436068b7287fd8bdf9a5d50a84c066
SitianHu <167514515+SitianHu@users.noreply.github.com>
2024-12-25 15:52:09 +0800

    更新奶茶选项

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const button = document.getElementById('decide-button');
 const result = document.getElementById('result');
 
 // 定义食物列表
-const foods = ["西北风", "粑粑", "唐僧肉", "麦当劳"];
+const foods = ["喜茶", "茶颜", "一点点", "coco", "茶百道", "蜜雪冰城", "茶话弄",
+               "霸王茶姬", "茉沏", "茉莉奶白", "马伍旺", "奈雪", "KOI", "茶救星球",
+               "去茶山", "茶理宜世", "百分茶", "古茗", "阿嬷手作", "柠檬向右",
+               "柠季", "茉酸奶", "7分甜", "书亦烧仙草", "沪上阿姨", "爷爷不泡茶",
+               "丘大叔", "益禾堂", "乐乐茶"];
 
 // 按钮点击事件
 button.addEventListener('click', () => {
